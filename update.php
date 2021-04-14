2bfd40f24395bca80c9284c1ea9bb6fbc0d506a1
PRAVEEN KUMAR S <79149661+PraveenKodanakatte@users.noreply.github.com>
2021-04-14 15:27:23 +0530

    Add files via upload

---
+++ update.php
@@ -0,0 +1,49 @@
+<?php
+
+  $conn = mysqli_connect("localhost","root","","firstdata");
+  error_reporting(0);
+    $fn = $_GET['fn'];
+    $Mb =  $_GET['Mb'];
+    $Ag =  $_GET['Ag'];
+    ?>
+<html>
+    <head>
+        <title>Update</title>
+    </head>
+
+</html>
+<body>
+<form action="" method="GET">
+<table border="0">
+<tr>
+    <td>Name</td>
+    <td><input type="text" value="<?php  echo "$fn" ?>" name="Name" required></td>
+</tr>
+<tr>
+    <td>Mobile</td>
+    <td><input type="text" value="<?php  echo "$Mb" ?>" name="Mobile" required></td>
+</tr>
+<tr>
+    <td>Age</td>
+    <td><input type="text" value="<?php  echo "$Ag" ?>" name="Age" required></td>
+</tr>
+<tr>
+    <td colspan="2" align="center"><input type="submit"  name="submit" value="Update"></td>
+</tr>
+<?php
+ $conn = mysqli_connect("localhost","root","","firstdata");
+ error_reporting(0);
+if($_GET['submit'])
+{
+    $Name= $_GET['Name'];
+    $Mobile= $_GET['Mobile'];
+    $Age= $_GET['Age'];
+
+    $query = "UPDATE INFO SET Name= '$Name',Mobile = '$Mobile', Age = '$Age' WHERE id = '$row'";
+    $data=mysqli_query($conn,$query);
+}
+?>
+</table>
+</body>
+</html>
+
